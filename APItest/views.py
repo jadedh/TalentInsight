f71f9b1b23991bb936a2eb9ed06f04e89ca95af7
jadedh <jadedh@bu.edu>
2018-02-16 01:26:19 -0500

    TalentIQ API test

--- APItest/views.py
+++ APItest/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    """return HttpResponse('Hello, World!')"""
+    response = requests.get('http://freegeoip.net/json/')
+    geodata = response.json()
+    return render(request, 'home.html',{
+        'ip': geodata['ip'],
+        'country': geodata['country_name'],
+        'api_key': '',
+    })
+    """, {'boards': boards})"""
